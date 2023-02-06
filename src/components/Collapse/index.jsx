7ced96fbb690509915fe9b1918c686fb9a10e2d7
Bazabas Barbara <barbara-bazabas@live.fr>
2023-02-06 16:17:47 +0000

    apart about -> fixed collapse nesting

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -21,13 +21,13 @@ export default function Collapse( props ) {
 
     return <>
         <div className='collapse' style={ { visibility: 'visible' } }>
-            <button className={ currentPage === 'about' ? 'collapseBtnAb' : 'collapseBtnAp' } onClick={ toggle }>
+            <button className={ currentPage === 'about' ? 'collapseBtnAb' : 'collapseBtnAp' } style={ { position: 'relative', zIndex: '10' } } onClick={ toggle }>
                 <p>{ props.label }</p>
                 { open ? ( <FontAwesomeIcon icon={ faChevronUp } className="chevron" /> ) : ( <FontAwesomeIcon icon={ faChevronDown } className="chevron" /> ) }
             </button>
             <div className='contentParent'
                 ref={ contentRef }
-                style={ { height: open ? contentRef.current.scrollHeight + 10 + 'px' : '0px' } }>
+                style={ { height: open ? contentRef.current.scrollHeight + 10 + 'px' : '0px', position: 'relative', zIndex: '-10', bottom: '16px' } }>
                 <div className={ currentPage === 'about' ? 'contentAb' : 'contentAp' }>{ props.children }</div>
             </div>
         </div>
